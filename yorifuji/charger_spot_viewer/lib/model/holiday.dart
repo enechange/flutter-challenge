cc2c4fb5bb00dc7158e3bdf3b0ebb8e3d26b975d
yorifuji <yorifuji@gmail.com>
2022-01-30 01:18:15 +0900

    祝日を判定するClassを追加 #4

---
+++ yorifuji/charger_spot_viewer/lib/model/holiday.dart
@@ -0,0 +1,79 @@
+/*
+holiday.dart: 2022年の祝日を判定
+
+内閣府
+https://www8.cao.go.jp/chosei/shukujitsu/gaiyou.html
+
+2022/1/1	元日
+2022/1/10	成人の日
+2022/2/11	建国記念の日
+2022/2/23	天皇誕生日
+2022/3/21	春分の日
+2022/4/29	昭和の日
+2022/5/3	憲法記念日
+2022/5/4	みどりの日
+2022/5/5	こどもの日
+2022/7/18	海の日
+2022/8/11	山の日
+2022/9/19	敬老の日
+2022/9/23	秋分の日
+2022/10/10	スポーツの日
+2022/11/3	文化の日
+2022/11/23	勤労感謝の日
+*/
+
+class Holiday {
+  final int year;
+  final int month;
+  final int day;
+  Holiday(this.year, this.month, this.day);
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (other is Holiday) {
+      return runtimeType == other.runtimeType &&
+          year == other.year &&
+          month == other.month &&
+          day == other.day;
+    } else {
+      return false;
+    }
+  }
+
+  @override
+  int get hashCode =>
+      (year.toString() + month.toString() + day.toString()).hashCode;
+}
+
+class HolidayJP {
+  final List<Holiday> _holidays = [];
+  HolidayJP() {
+    _holidays.add(Holiday(2022, 1, 1));
+    _holidays.add(Holiday(2022, 1, 10));
+    _holidays.add(Holiday(2022, 2, 11));
+    _holidays.add(Holiday(2022, 2, 23));
+    _holidays.add(Holiday(2022, 3, 21));
+    _holidays.add(Holiday(2022, 4, 29));
+    _holidays.add(Holiday(2022, 5, 3));
+    _holidays.add(Holiday(2022, 5, 4));
+    _holidays.add(Holiday(2022, 5, 5));
+    _holidays.add(Holiday(2022, 7, 18));
+    _holidays.add(Holiday(2022, 8, 11));
+    _holidays.add(Holiday(2022, 9, 19));
+    _holidays.add(Holiday(2022, 9, 23));
+    _holidays.add(Holiday(2022, 10, 10));
+    _holidays.add(Holiday(2022, 11, 3));
+    _holidays.add(Holiday(2022, 11, 23));
+  }
+
+  bool isHoliday({required int year, required int month, required int day}) {
+    return _holidays.contains(Holiday(year, month, day));
+  }
+
+  bool isHolidayToday({now}) {
+    now ??= DateTime.now();
+    return _holidays.contains(Holiday(now.year, now.month, now.day));
+  }
+}
---
+++ yorifuji/charger_spot_viewer/test/holiday_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:charger_spot_viewer/model/holiday.dart';
+
+void main() {
+  group('Holiday Class', () {
+    test('override equals operator, same object', () {
+      expect(Holiday(2022, 1, 1) == Holiday(2022, 1, 1), isTrue);
+    });
+    test('override equals operator, different object', () {
+      expect(Holiday(2022, 1, 1) == Holiday(2022, 1, 2), isFalse);
+    });
+  });
+  group('HolidayJP Class', () {
+    test('is holiday', () {
+      expect(HolidayJP().isHoliday(year: 2022, month: 1, day: 1), isTrue);
+      expect(HolidayJP().isHoliday(year: 2022, month: 11, day: 23), isTrue);
+
+      // expect(HolidayJP().isHolidayToday(), isTrue); // depends on the current date.
+      expect(HolidayJP().isHolidayToday(now: DateTime.utc(2022, 1, 1)), isTrue);
+      expect(
+          HolidayJP().isHolidayToday(now: DateTime.utc(2022, 1, 2)), isFalse);
+    });
+    test('not holiday', () {
+      expect(HolidayJP().isHoliday(year: 2022, month: 1, day: 2), isFalse);
+    });
+  });
+}
