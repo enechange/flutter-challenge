1411edb40e41ebb0e3e4bc72422a3bcf9bc70d94
yorifuji <yorifuji@gmail.com>
2022-02-02 00:46:12 +0900

    リストで選択したスポット中心としたマップを表示 #5

--- yorifuji/charger_spot_viewer/lib/widget/charger_spot_map_screen.dart
+++ yorifuji/charger_spot_viewer/lib/widget/charger_spot_map_screen.dart
@@ -1,27 +1,67 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:charger_spot_viewer/model/charger_spot_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 const appBarTitle = 'Charger Spot Viewer';
+const defaultZoomLevel = 15.0;
 
-class ChargerSpotMapScreen extends StatelessWidget {
-  // In the constructor, require a Todo.
+class ChargerSpotMapScreen extends StatefulWidget {
   const ChargerSpotMapScreen({Key? key, required this.chargerSpot})
       : super(key: key);
 
-  // Declare a field that holds the Todo.
   final ChargerSpot chargerSpot;
 
   @override
+  State<ChargerSpotMapScreen> createState() => _ChargerSpotMapScreen();
+}
+
+class _ChargerSpotMapScreen extends State<ChargerSpotMapScreen> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  final Set<Marker> _markers = {};
+
+  @override
   Widget build(BuildContext context) {
-    // Use the Todo to create the UI.
     return Scaffold(
       appBar: AppBar(
         title: const Text(appBarTitle),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Text(chargerSpot.name!),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+            target: LatLng(
+                widget.chargerSpot.latitude!, widget.chargerSpot.longitude!),
+            zoom: defaultZoomLevel),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+          _createMarker();
+        },
+        markers: _markers,
+        myLocationEnabled: false,
+        myLocationButtonEnabled: false,
       ),
     );
+  }
+
+  void _createMarker() {
+    setState(() {
+      _markers.add(Marker(
+          onTap: () async {
+            final GoogleMapController controller = await _controller.future;
+            final visibleRegion = await controller.getVisibleRegion();
+            print(visibleRegion.northeast);
+            print(visibleRegion.southwest);
+          },
+          markerId: MarkerId(widget.chargerSpot.name!),
+          icon:
+              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+          position: LatLng(
+              widget.chargerSpot.latitude!, widget.chargerSpot.longitude!),
+          infoWindow: InfoWindow(
+              title: widget.chargerSpot.name!,
+              snippet:
+                  '利用可能台数：${widget.chargerSpot.chargerDevices!.length.toString()}')));
+    });
   }
 }
