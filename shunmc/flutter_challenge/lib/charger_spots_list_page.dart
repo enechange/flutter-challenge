55a23d4f477968f8fd1f1d53fa31a0e8547b9513
ShunMc <news_two_three@yahoo.co.jp>
2022-10-02 18:52:38 +0900

    WebAPIから充電スポットの情報を取得して表示する

--- shunmc/flutter_challenge/lib/charger_spots_list_page.dart
+++ shunmc/flutter_challenge/lib/charger_spots_list_page.dart
@@ -1,21 +1,44 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_challenge/charget_spot_card.dart';
+import 'package:openapi/openapi.dart';
 
 class ChargerSpotsListPage extends StatelessWidget {
   const ChargerSpotsListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final openApi =
+        Openapi(dio: Dio(BaseOptions(baseUrl: 'https://stg.evene.jp/')));
+    final repository = openApi.getChargerSpotsApi();
+    final response = repository.chargerSpots(
+      X_EVENE_NATIVE_API_TOKEN: const String.fromEnvironment('API_TOKEN'),
+      swLat: '35.683331703634124',
+      swLng: '139.7657155055581',
+      neLat: '35.686849507072736',
+      neLng: '139.77340835691592',
+    );
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 32),
-          child: Column(
-            children: [
-              ChargerSpotCard(),
-            ],
-          ),
-        ),
+        child: FutureBuilder(
+            future: response,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.data == null || snapshot.data!.data == null) {
+                return Text('error');
+              }
+              return SingleChildScrollView(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 32, vertical: 22),
+                child: Column(
+                  children: snapshot.data!.data!.chargerSpots
+                      .map((e) => ChargerSpotCard(e, key: ValueKey(e.uuid)))
+                      .toList(),
+                ),
+              );
+            }),
       ),
     );
   }
--- shunmc/flutter_challenge/lib/charget_spot_card.dart
+++ shunmc/flutter_challenge/lib/charget_spot_card.dart
@@ -1,99 +1,112 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_challenge/gen/assets.gen.dart';
+import 'package:openapi/openapi.dart';
 
 class ChargerSpotCard extends StatelessWidget {
-  const ChargerSpotCard({super.key});
+  const ChargerSpotCard(this.chargerSpot, {super.key});
+
+  final ChargerSpot chargerSpot;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        boxShadow: [
-          BoxShadow(
-            color: Color(0x26000000),
-            blurRadius: 29,
-            offset: Offset(0, 5),
-          ),
-        ],
-        color: Colors.white,
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(12),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 72,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Image.network(
-                      'https://img01.jalannews.jp/img/2020/04/20200415_kouzu-photo_13-670x443.jpg',
-                      fit: BoxFit.fitWidth,
-                    ),
-                  ),
-                  SizedBox(width: 4),
-                  Expanded(
-                    child: Image.network(
-                      'https://img01.jalannews.jp/img/2020/04/20200415_kouzu-photo_13-670x443.jpg',
-                      fit: BoxFit.fitWidth,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('八景島シーパラダイス'),
-                  Table(
-                    defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                    columnWidths: const {
-                      0: FixedColumnWidth(12),
-                      1: FixedColumnWidth(80),
-                    },
-                    children: [
-                      TableRow(
-                        children: [
-                          Assets.images.iconPowerPng.image(),
-                          Text('利用可能'),
-                          Text('12台'),
-                        ],
-                      ),
-                      TableRow(
-                        children: [
-                          Assets.images.iconBoltPng.image(),
-                          Text('充電出力'),
-                          Text('4.8kW'),
-                        ],
-                      ),
-                      TableRow(
-                        children: [
-                          Assets.images.iconWatchPng.image(),
-                          Text('営業中'),
-                          Text('10:00 - 19:00'),
-                        ],
-                      ),
-                      TableRow(
-                        children: [
-                          Assets.images.iconTodayPng.image(),
-                          Text('定休日'),
-                          Text('祝日'),
-                        ],
-                      ),
-                    ],
-                  ),
-                  TextButton(
-                    onPressed: null,
-                    child: Text('地図アプリで経路を見る'),
-                  ),
-                ],
-              ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              color: Color(0x26000000),
+              blurRadius: 29,
+              offset: Offset(0, 5),
             ),
           ],
+          color: Colors.white,
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 60,
+                child: Row(
+                  children:
+                      chargerSpot.images == null || chargerSpot.images!.isEmpty
+                          ? [
+                              Expanded(
+                                child: Assets.images.noimage.image(
+                                  fit: BoxFit.fitWidth,
+                                ),
+                              ),
+                            ]
+                          : chargerSpot.images!
+                              .map(
+                                (e) => Expanded(
+                                  child: Image.network(
+                                    e.url,
+                                    fit: BoxFit.fitWidth,
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(chargerSpot.name),
+                    Table(
+                      defaultVerticalAlignment:
+                          TableCellVerticalAlignment.middle,
+                      columnWidths: const {
+                        0: FixedColumnWidth(12),
+                        1: FixedColumnWidth(80),
+                      },
+                      children: [
+                        TableRow(
+                          children: [
+                            Assets.images.iconPowerPng.image(),
+                            Text('利用可能'),
+                            Text('${chargerSpot.chargerDevices?.length}台'),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            Assets.images.iconBoltPng.image(),
+                            Text('充電出力'),
+                            Text(
+                                '${chargerSpot.chargerDevices?.first.power}kW'),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            Assets.images.iconWatchPng.image(),
+                            Text(chargerSpot.nowAvailable?.toString() ?? ''),
+                            Text(
+                                '${chargerSpot.chargerSpotServiceTimes?.first.startTime} - ${chargerSpot.chargerSpotServiceTimes?.first.endTime}'),
+                          ],
+                        ),
+                        TableRow(
+                          children: [
+                            Assets.images.iconTodayPng.image(),
+                            Text('定休日'),
+                            Text('祝日'),
+                          ],
+                        ),
+                      ],
+                    ),
+                    TextButton(
+                      onPressed: null,
+                      child: Text('地図アプリで経路を見る'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
