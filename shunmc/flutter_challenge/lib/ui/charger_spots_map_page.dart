6dd55cdd4b32ebae9e307cd42102d40e577dcbb2
ShunMc <news_two_three@yahoo.co.jp>
2022-10-03 21:41:42 +0900

    androidでdart-defineで指定したGoogleMapのApiKeyを使用する

--- shunmc/flutter_challenge/lib/ui/charger_spots_map_page.dart
+++ shunmc/flutter_challenge/lib/ui/charger_spots_map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:openapi/openapi.dart';
 
 class ChargerSpotsMapPage extends StatelessWidget {
@@ -10,8 +11,13 @@ class ChargerSpotsMapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text(chargerSpot.name),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            chargerSpot.latitude.toDouble(),
+            chargerSpot.longitude.toDouble(),
+          ),
+        ),
       ),
     );
   }
