598a0a3d3a222e8109d4b1694e9e38d8057f4650
ShunMc <news_two_three@yahoo.co.jp>
2022-10-10 01:54:53 +0900

    ChargerSpotの取得のインターフェイスを修正

--- shunmc/flutter_challenge/lib/data/charger_spots_repository.dart
+++ shunmc/flutter_challenge/lib/data/charger_spots_repository.dart
@@ -1,22 +1,18 @@
 import 'package:dio/dio.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:openapi/openapi.dart';
 
 class ChargerSpotsRepository {
-  Future<Iterable<ChargerSpot>?> getChargerSpots({
-    required String swLat,
-    required String swLng,
-    required String neLat,
-    required String neLng,
-  }) async {
+  Future<Iterable<ChargerSpot>?> getChargerSpots(LatLngBounds bounds) async {
     final dio = Dio(BaseOptions(baseUrl: 'https://stg.evene.jp/'));
     final openApi = Openapi(dio: dio);
     final repository = openApi.getChargerSpotsApi();
     final response = await repository.chargerSpots(
       X_EVENE_NATIVE_API_TOKEN: const String.fromEnvironment('API_TOKEN'),
-      swLat: swLat,
-      swLng: swLng,
-      neLat: neLat,
-      neLng: neLng,
+      swLat: bounds.southwest.latitude.toString(),
+      swLng: bounds.southwest.longitude.toString(),
+      neLat: bounds.northeast.latitude.toString(),
+      neLng: bounds.northeast.longitude.toString(),
     );
     return response.data?.chargerSpots;
   }
--- shunmc/flutter_challenge/lib/ui/charger_spots_list_page.dart
+++ shunmc/flutter_challenge/lib/ui/charger_spots_list_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_challenge/data/charger_spots_repository.dart';
 import 'package:flutter_challenge/ui/charget_spot_card.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ChargerSpotsListPage extends StatelessWidget {
   const ChargerSpotsListPage({super.key});
@@ -9,12 +10,11 @@ class ChargerSpotsListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final repository = ChargerSpotsRepository();
-    final response = repository.getChargerSpots(
-      swLat: '35.683331703634124',
-      swLng: '139.7657155055581',
-      neLat: '35.686849507072736',
-      neLng: '139.77340835691592',
+    final latLngBounds = LatLngBounds(
+      southwest: const LatLng(35.683331703634124, 139.7657155055581),
+      northeast: const LatLng(35.686849507072736, 139.77340835691592),
     );
+    final response = repository.getChargerSpots(latLngBounds);
     return Scaffold(
       body: SafeArea(
         child: FutureBuilder(
