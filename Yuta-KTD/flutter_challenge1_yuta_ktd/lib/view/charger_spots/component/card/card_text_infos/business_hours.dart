10ada28b5f6b05e60ae0c99e390230f2c345a839
Yuta-KTD <yuta.katada.2@gmail.com>
2023-09-11 23:52:59 +0900

    営業時間情報が存在しない、複数存在する場合を考慮する

--- Yuta-KTD/flutter_challenge1_yuta_ktd/lib/view/charger_spots/component/card/card_text_infos/business_hours.dart
+++ Yuta-KTD/flutter_challenge1_yuta_ktd/lib/view/charger_spots/component/card/card_text_infos/business_hours.dart
@@ -2,26 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:flutter_challenge1_yuta_ktd/constant/decolation_style.dart';
 import 'package:flutter_challenge1_yuta_ktd/gen/assets.gen.dart';
 import 'package:flutter_challenge1_yuta_ktd/view/charger_spots/component/card/card_text_infos/card_text_info_title.dart';
-import 'package:openapi/models.dart';
+import 'package:openapi/model/charger_spot_service_time.dart';
+import 'package:openapi/model/now_available.dart';
 
 import '../card_text.dart';
 
 /// 営業時間に関する情報を出力する
 class BusinessHours extends StatelessWidget {
   final List<ChargerSpotServiceTime> chargerSpotServiceTimes;
-  const BusinessHours(this.chargerSpotServiceTimes, {super.key});
+  final NowAvailable? nowAvailable;
+  const BusinessHours(
+    this.chargerSpotServiceTimes,
+    this.nowAvailable, {
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    final ChargerSpotServiceTime todayServiceTime =
-        chargerSpotServiceTimes.singleWhere((element) => element.today);
+    final List<ChargerSpotServiceTime> todayServiceTimes =
+        chargerSpotServiceTimes.where((element) => element.today).toList();
 
-    final isBusinessDay = todayServiceTime.businessDay == BusinessDay.yes;
-    final startTime = todayServiceTime.startTime;
-    final endTime = todayServiceTime.endTime;
-    final String serviceTime = (startTime == null || endTime == null)
-        ? ''
-        : '${todayServiceTime.startTime} - ${todayServiceTime.endTime}';
+    // 今日の曜日に関する営業時間情報がない場合
+    if (todayServiceTimes.isEmpty) {
+      return CardTextInfoTitle(
+        title: '営業時間外',
+        color: unavailableColor,
+        image: Assets.watchLator.image(width: 16.0, height: 16.0),
+      );
+    }
+
+    // スポットが営業中かどうか
+    final bool available =
+        (nowAvailable != null) && (nowAvailable == NowAvailable.yes);
+    //　営業時間情報が複数存在する場合を考慮し、営業時間に関する配列を作る
+    final List<String> serviceTimes = [];
+    for (var todayServiceTime in todayServiceTimes) {
+      final startTime = todayServiceTime.startTime;
+      final endTime = todayServiceTime.endTime;
+      final String? serviceTime = (startTime == null || endTime == null)
+          ? null
+          : '${todayServiceTime.startTime} - ${todayServiceTime.endTime}';
+      if (serviceTime != null) {
+        serviceTimes.add(serviceTime);
+      }
+    }
 
     return SizedBox(
       height: 19.0,
@@ -29,11 +53,22 @@ class BusinessHours extends StatelessWidget {
         children: [
           CardTextInfoTitle(
             // unKnownの場合も営業時間外とする
-            title: isBusinessDay ? '営業中' : '営業時間外',
-            color: isBusinessDay ? availableColor : unavailableColor,
+            title: available ? '営業中' : '営業時間外',
+            color: available ? availableColor : unavailableColor,
             image: Assets.watchLator.image(width: 16.0, height: 16.0),
           ),
-          CardText(serviceTime),
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: serviceTimes.length,
+              itemBuilder: (_, index) {
+                bool isLast = index == serviceTimes.length - 1;
+                final serviceTimeText =
+                    isLast ? serviceTimes[index] : '${serviceTimes[index]}、';
+                return CardText(serviceTimeText);
+              },
+            ),
+          ),
         ],
       ),
     );
